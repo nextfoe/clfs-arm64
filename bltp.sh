a8358ab430559558daf162c478fc892ca134a593
zhizhou <zhizhou.zh@gmail.com>
2015-12-30 16:37:46 +0800

    add build ltp script
    can't build pass because of:
    aarch64-linux-gnu-as: unrecognized option '-rc'
    need to work more..

---
+++ bltp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INSTALL=$(pwd)/root/mnt
+
+if [ ! -d ltp ]; then
+  git clone https://github.com/linux-test-project/ltp.git
+fi
+
+CROSS_COMPILE=aarch64-linux-gnu-
+HOST=aarch64-linux-gnu
+export CC=${CROSS_COMPILE}gcc
+export LD=${CROSS_COMPILE}ld
+export AR=${CROSS_COMPILE}ar
+export AR=${CROSS_COMPILE}as
+export RANDLIB=${CROSS_COMPILE}randlib
+export STRIP=${CROSS_COMPILE}strip
+export CXX=${CROSS_COMPILE}g++
+export LDFLAGS=-static
+export LIBS=-lpthread
+
+pushd ltp
+  make autotools
+  ./configure --host=${HOST} --prefix=${INSTALL} || exit
+  make || exit
+  make install
+popd
