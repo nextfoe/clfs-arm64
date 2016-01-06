c409a257250505691428a6179c73fdd9fa922f86
zhizhou <zhizhou.zh@gmail.com>
2016-01-06 15:38:24 +0800

    tiny change to run-parameter

--- source_me.sh
+++ source_me.sh
@@ -38,7 +38,7 @@ run() {
 if [ "is$BUILD_BUSYBOX_STATIC" == "isyes" ]; then
   PARAM="-initrd $ROOTFS"
 else
-  PARAM="--append root=/dev/vda -drive file=$ROOTFS,index=0,media=disk,format=raw"
+  PARAM="--append root=/dev/vda -drive file=$ROOTFS,media=disk,format=raw"
 
 fi
   qemu-system-aarch64 \
