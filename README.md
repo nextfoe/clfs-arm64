86a6f7d357637522047669bc271a632835b7d993
Zhizhou Zhang <zhizhou.zh@gmail.com>
2016-06-10 17:29:00 +0800

    rewrite this script
    - use busybox (static linked);
    - rename variables;
    - remove a lot packages;
    - move sysroot to tools

---
+++ README.md
@@ -0,0 +1,8 @@
+
+### prerequisite packages:
+
+- fedora:
+    dnf install flex libfdt-devel bison texinfo libtool gcc-g++
+    (To find a missing package name: dnf whatprovides */<program>)
+- ubuntu:
+    apt-get install libglib2.0-dev libpixman-1-dev libfdt-dev zlib1g-dev texinfo bison flex
--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ if [ $FORCE_UPDATE -eq 1 ]; then
 fi
 
 # build toolchain
-if [ ! -f $TOPDIR/tools/bin/aarch64-linux-gnu-gcc ]; then
+if [ ! -f $TOOLDIR/bin/aarch64-linux-gnu-gcc ]; then
   build_toolchain || die "build_toolchain"
 fi
 
@@ -65,42 +65,21 @@ if [ $? -ne 0 ]; then
 fi
 
 # build kernel
-if [ ! -f $TOPDIR/target/Image ]; then
+if [ ! -f $TOPDIR/out/Image ]; then
   build_kernel || die "build_kernel"
 fi
 
 
 if [ ! -f $SYSIMG ]; then
-  mkdir -p $SYSROOT/{bin,sbin,etc,run,dev,tmp,sys,proc,mnt,var,home,root,lib,usr/lib}
+  mkdir -p $SYSTEM/{dev,tmp,sys,proc,mnt,lib,usr/lib}
   prepare_build_env
-  test -f $SYSROOT/usr/lib64/libz.so || build_zlib || die "build_zlib"
-  test -f $SYSROOT/usr/lib64/libcheck.so || build_check || die "build_check"
-  test -f $SYSROOT/usr/lib64/libpam.so || build_pam || die "build_pam"
-  test -f $SYSROOT/usr/lib64/libcap.so || build_libcap || die "build_libcap"
-  test -f $SYSROOT/usr/lib64/libncurses.so || build_ncurses || die "build_ncurses"
-  test -f $SYSROOT/usr/bin/gcc || build_gcc || die "build_gcc"
-  test -f $SYSROOT/sbin/agetty || build_util_linux || die "build_util_linux"
-  test -f $SYSROOT/usr/bin/gdb ||  build_binutils_gdb || die "build_binutils_gdb"
-  test -f $SYSROOT/bin/bash ||  build_bash || die "build_bash"
-  test -f $SYSROOT/usr/bin/yes || build_coreutils || die "build_coreutils"
-  test -f $SYSROOT/usr/bin/strace ||  build_strace || die "build_strace"
-  test -f $SYSROOT/usr/bin/login || build_shadow || die "build_shadow"
-  test -f $SYSROOT/usr/bin/ps || build_procps || die "build_procps"
-  test -f $SYSROOT/sbin/udevd || build_eudev || die "build_eudev"
-  test -f $SYSROOT/sbin/init || build_sysvinit || die "build_sysvinit"
-  test -f $SYSROOT/bin/find || build_find || die "build_find"
-  test -f $SYSROOT/usr/bin/file || build_file || die "build_file"
-  test -f $SYSROOT/bin/grep || build_grep || die "build_grep"
-  test -f $SYSROOT/bin/sed || build_sed || die "build_sed"
-  test -f $SYSROOT/bin/awk || build_awk || die "build_awk"
-  test -f $SYSROOT/bin/gzip || build_gzip || die "build_gzip"
-  test -f $SYSROOT/bin/loadkeys || build_kbd || die "build_kbd"
-  test -f $SYSROOT/usr/bin/vim || build_vim || die "build_vim"
-  test -f $SYSROOT/etc/rc.d/init.d/rc || build_bootscript || die "build_bootscript"
-#  test -d $SYSROOT/opt/ltp || build_ltp || exit
+#  test -f $SYSTEM/usr/bin/strace ||  build_strace || die "build_strace"
+#  test -f $SYSTEM/usr/bin/file || build_file || die "build_file"
+#  test -f $SYSTEM/bin/gdb ||  build_binutils_gdb || die "build_binutils_gdb"
+#  test -f $SYSTEM/bin/bash ||  build_bash || die "build_bash"
+  test -f $SYSTEM/bin/busybox || build_busybox || die "build_busybox"
   clean_build_env
-  cp -f $TOPDIR/misc/etc/* $SYSROOT/etc/
-  new_disk $SYSIMG 2000
+  new_disk $SYSIMG 512
 fi
 
 # force build
